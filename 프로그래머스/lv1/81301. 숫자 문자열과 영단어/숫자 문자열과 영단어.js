711f2c8c92c45aa0cbb1527f195db9c657b49a47
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-10-27 21:46:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -2,13 +2,14 @@ function solution(s) {
     // 영어 -> 숫자
     // 문자열 교체 방법
     // replaceAll 메서드
-    // 반복문으로 split('') 배열화하여 인덱스 값 건너뛰기?
+    // 특정 문자열 기준으로 split 후 join
     
     const numArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     let result = s;
     
     numArr.forEach((el,idx)=>{
-        result = result.replaceAll(el, idx)
+        const splitByNum = result.split(el);
+        result = splitByNum.join(idx);
     })
     
     return Number(result);
