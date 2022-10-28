f90d202e473b1ba984a3c9402084b6ef97eda21b
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-10-28 22:35:16 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,6 @@
 function solution(spell, dic) {
     const alignSpell = spell.sort().join('')
-    const filteredArr = dic.filter((el)=>
-        el.length === alignSpell.length
-    )
-    
-    for(const text of filteredArr){
+    for(const text of dic){
         const alignedText = text.split('').sort().join('');
         if(alignedText === alignSpell){
             return 1;
