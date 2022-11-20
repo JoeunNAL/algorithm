4fe3cb116e431fc5acb8b677314197e23452ec01
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-11-20 10:25:40 +0900

    Update 저주의 숫자 3.js

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,10 +1,22 @@
 function solution(n) {
     let result = 0;
-    for(let i = 0; i<n; i++ ){
+    while(n>0){
         result++
-        while(result%3 === 0 || result.toString().includes('3')){
-            result++
+        if(result%3 ===0 || result.toString().includes('3')){
+           continue;
         }
+        n--
     }
     return result;
 }
+
+// function solution(n) {
+//     let result = 0;
+//     for(let i = 0; i<n; i++ ){
+//         result++
+//         while(result%3 === 0 || result.toString().includes('3')){
+//             result++
+//         }
+//     }
+//     return result;
+// }
