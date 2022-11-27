4d4b9133f963d3f75cdf826c811f83b637d9b3af
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-11-27 16:43:23 +0900

    Update 이진수 더하기.js

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -2,3 +2,14 @@ function solution(bin1, bin2) {
     const sum = parseInt(bin1,2)+ parseInt(bin2,2)
     return sum.toString(2)
 }
+
+// 이진법 구하는 함수
+// function getBineryString(target, num=10){
+//     let result="";
+
+//     while(target>0){
+//         result = target%num + result;
+//         target = Math.floor(target/num);
+//     }
+//     return result;
+// }
