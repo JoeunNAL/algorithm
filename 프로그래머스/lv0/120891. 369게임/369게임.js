bc78b8147ea79d97be6b2cc2c7c412d3dc7f6bcd
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-11-28 23:53:13 +0900

    Update 369게임.js

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,7 +1,10 @@
 function solution(order) {
-    let filteredString = order.toString();
-    [3,6,9].map((el)=>{
-      filteredString = filteredString.replaceAll(el.toString(),'')  
-    })
-    return order.toString().length-filteredString.length
+    return order.toString().replaceAll(/[^369]/g,'').length
+    
+//     let filteredString = order.toString();
+//     [3,6,9].map((el)=>{
+//       filteredString = filteredString.replaceAll(el.toString(),'')  
+//     })
+//     return order.toString().length-filteredString.length
+
 }
