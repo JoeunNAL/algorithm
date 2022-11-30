f9dd36164fc6abd5e9d5aeb85b31e7960d38db8c
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-11-30 15:40:40 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -2,10 +2,9 @@ function solution(id_pw, db) {
 // db에서 0번째 인덱스가 id_pw의 0번째 요소가 같은 것을 찾는다.
 // 찾은 값의 1번째 요소를 비교하여 결과를 반환한다.
     const target = db.find((el)=>el[0]===id_pw[0]);
-    let result ="fail"
     if(target){
-        result = target[1] ===id_pw[1]? "login": "wrong pw";
+        return target[1] ===id_pw[1]? "login": "wrong pw";
+    } else {
+        return "fail"
     }
-        
-    return result
 }
