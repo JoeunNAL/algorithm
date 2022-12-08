00136030e8a94163454e347e879f02870c6d93f1
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-08 13:05:57 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,7 +1,3 @@
 function solution(s1, s2) {    
-    let count =0;
-    s1.forEach((el)=>{
-        s2.indexOf(el)>-1? count++: null;
-    })
-    return count
+    return  s1.filter((el)=>s2.indexOf(el)>-1).length
 }
