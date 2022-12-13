27163a6399122920c389abb98135122b345be236
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-13 23:27:08 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,10 +1,14 @@
-function solution(dots) {    
-    const sides = {};
-    const pointer = dots.pop();
-    dots.forEach(el=>{
-        el[0] === pointer[0]? 
-            sides.x = Math.abs(el[1]-pointer[1]):
-            sides.y = Math.abs(el[0]-pointer[0]);
-    })
-    return sides.x*sides.y
+function solution(dots) { 
+    const xPointers = dots.map(el=>el[0])
+    const yPointers = dots.map(el=>el[1])
+    return (Math.max(...xPointers)-Math.min(...xPointers))* (Math.max(...yPointers)-Math.min(...yPointers))
+    
+    // const sides = {};
+    // const pointer = dots.pop();
+    // dots.forEach(el=>{
+    //     el[0] === pointer[0]? 
+    //         sides.x = Math.abs(el[1]-pointer[1]):
+    //         sides.y = Math.abs(el[0]-pointer[0]);
+    // })
+    // return sides.x*sides.y
 }
