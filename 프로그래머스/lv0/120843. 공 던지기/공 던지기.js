7f9724d060bc7a7566b7d0bf96a2eadd7263aff0
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-22 23:01:24 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,10 +1,3 @@
 function solution(numbers, k) {
-    const length = numbers.length
-    let count = 1;
-    let index = 1;
-    while(count < k){
-        index = (index+2)%length         
-        count++;
-    }
-    return index;
+    return numbers[2*(k-1)%numbers.length]
 }
