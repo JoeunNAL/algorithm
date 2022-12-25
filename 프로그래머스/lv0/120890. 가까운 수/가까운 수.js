c1540464fb9752083691c4def0c7e3686abd5088
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-25 21:30:11 +0900

    [level 0] Title: 가까운 수, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,9 +1,11 @@
 function solution(array, n) {
-    array.push(n)
-    const sortArr = array.sort((a,b)=>a-b)
-    const nIndex = sortArr.indexOf(n)
-    const lowerGap = Math.abs(sortArr[nIndex-1]-n)
-    const upperGap = Math.abs(sortArr[nIndex+1]-n)
+    return array.sort((a,b)=> a-b).reduce((acc, cur)=> Math.abs(acc-n) <= Math.abs(cur-n) ? acc : cur)
     
-    return lowerGap > upperGap ? sortArr[nIndex+1] : sortArr[nIndex-1]
+//     array.push(n)
+//     const sortArr = array.sort((a,b)=>a-b)
+//     const nIndex = sortArr.indexOf(n)
+//     const lowerGap = Math.abs(sortArr[nIndex-1]-n)
+//     const upperGap = Math.abs(sortArr[nIndex+1]-n)
+    
+//     return lowerGap > upperGap ? sortArr[nIndex+1] : sortArr[nIndex-1]
 }
