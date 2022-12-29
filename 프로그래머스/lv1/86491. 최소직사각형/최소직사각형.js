daf2a8a9e81a53ef2482790857df47978b1cb67f
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-29 14:39:48 +0900

    [level 1] Title: 최소직사각형, Time: 6.51 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,4 @@
 function solution(sizes) {
-    let maxWidth =0;
-    let maxHeight =0;
-    sizes.forEach((el)=>{
-        let [width, height]=[...el].sort((a,b)=>b-a)
-        if(maxWidth<width) maxWidth = width;
-        if(maxHeight<height) maxHeight=height;
-    })
-    return maxWidth*maxHeight
+    const [maxWidth, maxHeight] = sizes.reduce(([w,h],[cw, ch])=>[Math.max(w, Math.max(cw,ch)),Math.max(h, Math.min(cw,ch))],[0,0])
+    return maxWidth* maxHeight
 }
