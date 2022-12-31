f4c43299776010c1a820ae6f9aa730f414f06177
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2022-12-31 14:58:26 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -3,12 +3,8 @@ function solution(k, m, score) {
     const alignScores = score.sort((a,b)=>b-a);
     
     for(let i = 0; i+m <= alignScores.length; i+=m){
-        sum+= alignScores.slice(i, i+m).at(-1)*m
+        sum+= alignScores[i+m-1]*m
     }
-    // while(alignScores.length>=m){
-    //     const newBox = alignScores.splice(0,m);
-    //     sum += newBox.at(-1)*m
-    // }
     
     return sum;
 }
