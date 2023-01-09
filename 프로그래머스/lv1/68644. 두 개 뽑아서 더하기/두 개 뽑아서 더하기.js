82a65a703eda400a6a9b1f37c7770a5c642b82a0
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-01-09 20:51:19 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    const set = new Set();
+    const length = numbers.length;
+    for(let i =0; i<length; i++){
+        for(let j = 0; j<length; j++){
+            if(j!==i)set.add(numbers[j]+numbers[i]);
+        }
+    }
+    return [...set].sort((a,b)=>a-b)
+}
