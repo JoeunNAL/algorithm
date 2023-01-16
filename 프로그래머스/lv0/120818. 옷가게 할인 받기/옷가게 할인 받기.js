f256698c21b6fce13dd618bb8c46dd425655316e
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-01-16 22:36:45 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,11 @@
 function solution(price) {
-    if(price>=500000) return Math.floor(price*0.8);
-    if(price>=300000) return Math.floor(price*0.9);
-    if(price>=100000) return Math.floor(price*0.95);
-    return Math.floor(price);
+    const discounts = [[500000, 0.2], [300000,0.1],[100000,0.05]];
+    let result = price;
+    for(let discount of discounts){
+        if(price>=discount[0]) {
+            result = price*(1-discount[1]);
+            break;
+        }
+    }
+    return Math.floor(result);
 }
