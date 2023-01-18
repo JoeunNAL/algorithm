25fc5953dd177cd3e49a6901cfc4527104cd2068
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-01-18 22:33:40 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,14 @@
 function solution(k, score) {
     const result =[];
-    let ranks = [];
+    const ranks = [];
     
     for(let i = 0; i < score.length; i++){
         ranks.push(score[i]);
         ranks.sort((a,b) => b-a);
-        if(i >= k){
-            result.push(ranks[k-1]);
-        }else{
-            result.push(ranks[ranks.length-1]);        
+        if(i>=k){
+            ranks.pop();
         }
+        result.push(ranks.at(-1));
     }
     
     return result;
