38cec50f56cf0c7e9945eb349ed2e66c70460d45
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-01-20 20:49:10 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,14 +1,4 @@
-function makeTernaryString(num){
-    let number = '';
-    let quotient = num;
-    while(quotient > 0){
-        number = quotient%3 + number;
-        quotient = parseInt(quotient/3,10);
-    }
-    return number;
-}
-
 function solution(n) {
-    const reverseString = makeTernaryString(n).split('').reverse().join('');
+    const reverseString = n.toString(3).split('').reverse().join('');
     return parseInt(reverseString,3)
 }
