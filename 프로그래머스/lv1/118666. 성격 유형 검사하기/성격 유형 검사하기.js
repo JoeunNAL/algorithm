3ad67b45e0bdb0a9dfc87afe26d7afc22df787b1
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-05 17:10:28 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -2,19 +2,15 @@ function solution(survey, choices) {
     const scores ={
         R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0,
     };
-    const categorys = ['RT', 'CF', 'JM', 'AN'];
+    const categorys = ['RT', 'CF', 'JM', 'AN']; // 비교대상 알파벳순 배치
     
-    survey.forEach((el,idx)=>{
-        const firstChar = el[0];    
-        const secondChar = el[1]; 
-        const choice = choices[idx];
+    choices.forEach((choice,idx)=>{
+        const [firstChar, secondChar] = survey[idx];    
         scores[ choice < 4 ? firstChar : secondChar ] += Math.abs(choice-4);
     });
     
     return categorys.reduce((acc,cur)=>{
-        const [firstChar, secondChar] = cur.split('').sort();
-        const win = scores[firstChar] >= scores[secondChar] ? 
-                     firstChar : secondChar; 
-        return acc + win;
+        const [firstChar, secondChar] = cur;
+        return acc + (scores[firstChar] >= scores[secondChar] ? firstChar : secondChar);
     },'')
 }
