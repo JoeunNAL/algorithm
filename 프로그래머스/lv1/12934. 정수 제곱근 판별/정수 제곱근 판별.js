15bc6c0d4824f58d2cc8603607600d67a3ad2666
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-14 18:52:55 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,4 +1,15 @@
 function solution(n) {
-    const rootValue = Math.sqrt(n);
-    return rootValue % 1 === 0 ? ( rootValue + 1 )**2 : -1;
+    // const rootValue = Math.sqrt(n);
+    // return rootValue % 1 === 0 ? ( rootValue + 1 )**2 : -1;
+
+    let num = 1, result = -1;
+    
+    while(num*num <= n){
+        if(num*num === n) {
+            result = (num+1)*(num+1);
+            break;
+        }
+        num++;
+    }
+    return result;
 }
