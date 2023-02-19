e97f7069ef600b4c0a1bb0cdf4a1c61a79679b70
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-19 23:23:10 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,12 +1,13 @@
 function solution(N, stages) {
     const result = [];
-    
-    for(let i = 0; i < N; i++){
-        const playerReached = stages.filter(el=> el>=i+1 ).length;
-        const playerInStage = stages.filter(el=> el===i+1 ).length;
-        result[i] = [i+1, playerInStage/playerReached];
+
+    for(let i = 1; i <= N; i++){
+        const arrivalCount = stages.filter(el=> el >= i).length;
+        const stayCount = stages.filter(el=> el === i).length;
+        const rate = arrivalCount === 0 ? 0: stayCount/arrivalCount;
+        result[i-1] = [i, rate];
     }
-    
+
     result.sort((a,b) => {
         if(b[1] === a[1]){
             return a[0] - b[0]
@@ -14,6 +15,6 @@ function solution(N, stages) {
             return b[1] - a[1]
         }
     });
-    
-    return result.map(([stage, rate])=> stage);
+
+    return result.map(([stage, _])=> stage);
 }
