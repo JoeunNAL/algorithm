786b4ba6ddaefaeb621145f145c45e63f3894ff0
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-20 17:52:53 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,14 +1,12 @@
 function solution(cards1, cards2, goal) {
+    // 시간 비교용 풀이
     let result = "Yes";
     
-    cards1.reverse();
-    cards2.reverse();
-    
     goal.forEach((el,idx)=>{
-        if(cards1.at(-1) === el){
-            cards1.pop();
-        } else if( cards2.at(-1) === el){
-            cards2.pop();
+        if(cards1[0] === el){
+            cards1.shift();
+        } else if( cards2[0] === el){
+            cards2.shift();
         } else {
             result = "No";
         }
