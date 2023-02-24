df9a26afd05f35d80127ef1ca9296f34de69c96f
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-24 22:51:36 +0900

    [unrated] Title: 귤 고르기, Time: 350.39 ms, Memory: 46 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,26 +1,18 @@
 function solution(k, tangerine) {
     let result = 0;
-    const sizeCount = tangerine.reduce((acc,cur)=>{
-        if(!acc[cur-1]) {
-            acc[cur-1] = 1;
-        } else {
-            acc[cur-1] += 1;
-        }
-        return acc;
-    },[]);
-    
-    // 있는 크기만 분류
-    const Counts = sizeCount.filter(el => el).sort((a,b)=>a-b);
+    const sizeCounts = tangerine.reduce((acc,cur)=>{
+                            acc[cur-1] = (acc[cur-1] || 0) + 1;
+                            return acc;
+                        },[]).filter(el => el);
+    sizeCounts.sort((a,b)=>a-b);
     
     while(k > 0){
         result++;
-        const countBigger = Counts.pop();        
-        if(k < countBigger){
-            k=0;
-            break; 
-        } else {
-            k -= countBigger;
-        }
+        
+        const countBigger = sizeCounts.pop();        
+        if(k < countBigger) { break };
+        k -= countBigger;
     }
+    
     return result;
 }
