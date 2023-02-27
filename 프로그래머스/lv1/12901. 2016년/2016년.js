94a569970ca02abcf81c4c3aeeda5bc2b5b902be
Eunjin Jo <90553688+JoeunNAL@users.noreply.github.com>
2023-02-27 22:52:21 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    const days = [31,29,31,30,31,30,31,31,30,31,30,31];
+    const week = ['THU','FRI','SAT','SUN','MON','TUE','WED'];
+    let totalDays = b;
+    for(let i = 0; i< a-1; i++){
+        totalDays += days[i];
+    }
+    return week[totalDays%7];
+}
